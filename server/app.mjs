9f75cb4812883360b86de96cde5032948ecf1bee
Sirapop Chanphui <srp073536@gmail.com>
2026-02-02 20:59:35 +0700

    feat: Enhance server functionality with CORS support and CRUD endpoints for assignments

--- server/app.mjs
+++ server/app.mjs
@@ -1,13 +1,108 @@
 import express from "express";
 import connectionPool from "./utils/db.mjs";
+import cors from "cors";
 
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json())
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Frontend local (Vite)
+      "https://your-frontend.vercel.app", // Frontend ที่ Deploy แล้ว
+      // ✅ ให้เปลี่ยน https://your-frontend.vercel.app เป็น URL จริงของ Frontend ที่ deploy แล้ว
+    ],
+  })
+);
+
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+//get all assignments
+app.get("/assignments", async (req, res) => {
+  try {
+    const result = await connectionPool.query(`SELECT * FROM assignments`)
+    res.status(200).json({ assignments: result.rows });
+  } catch (error) {
+    console.error("Create assignment error:", error);
+
+    return res.status(500).json({ "message": "Server could not read assignment because database connection" });
+  }
+});
+
+//get assignment by id
+app.get("/assignments/:assignmentId", async (req, res) => {
+  try {
+    const { assignmentId } = req.params;
+    const result = await connectionPool.query(`SELECT * FROM assignments WHERE assignment_id = $1`, [assignmentId])
+    console.log(result)
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        "message": "Server could not find a requested assignment"
+      });
+    }
+    res.status(200).json({ assignments: result.rows[0] });
+  } catch (error) {
+    console.error("Create assignment error:", error);
+
+    return res.status(500).json({ "message": "Server could not read assignment because database connection" });
+  }
+});
+
+//update assignment by id
+app.put("/assignments/:assignmentId", async (req, res) => {
+  try {
+    const { assignmentId } = req.params;
+    const { title, content, category } = req.body;
+
+
+    // 1. เช็กว่ามี assignment นี้ไหม
+    const checkResult = await connectionPool.query(
+      "SELECT * FROM assignments WHERE assignment_id = $1",
+      [assignmentId]
+    );
+
+    if (checkResult.rows.length === 0) {
+      return res.status(404).json({
+        message: "Server could not find a requested assignment"
+      });
+    }
+
+    // 2. update assignment
+    const result = await connectionPool.query(`UPDATE assignments SET title = $1, content = $2, category = $3, updated_at = NOW() WHERE assignment_id = $4`, [title, content, category, assignmentId])
+    console.log(result)
+    res.status(200).json({ "message": "Updated assignment successfully" });
+  } catch (error) {
+    console.error("Create assignment error:", error);
+
+    return res.status(500).json({ "message": "Server could not update assignment because database connection" });
+  }
+});
+
+//update assignment by id
+app.delete("/assignments/:assignmentId", async (req, res) => {
+  try {
+    const { assignmentId } = req.params;
+
+    const checkResult = await connectionPool.query(
+      "SELECT * FROM assignments WHERE assignment_id = $1",
+      [assignmentId]
+    );
+
+    if (checkResult.rows.length === 0) {
+      return res.status(404).json({ message: "Server could not find a requested assignment" });
+    }
+
+    const result = await connectionPool.query(`DELETE FROM assignments WHERE assignment_id = $1`, [assignmentId])
+    res.status(200).json({ "message": "Deleted assignment successfully" });
+  } catch (error) {
+    console.error("Create assignment error:", error);
+
+    return res.status(500).json({ "message": "Server could not delete assignment because database connection" });
+  }
 });
 
 app.post("/assignments", async (req, res) => {
@@ -48,6 +143,10 @@ app.post("/assignments", async (req, res) => {
 
 })
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
 });
